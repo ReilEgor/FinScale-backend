be7e5aca6016f0d33abbe4d782859359ab4923da
YehorReil <egorrejl44@gmail.com>
2026-03-23 21:39:37 +0200

    feat: add mocks to CurrencyService  using mockery

--- services/CurrencyService/internal/domain/currency.go
+++ services/CurrencyService/internal/domain/currency.go
@@ -8,11 +8,13 @@ type ConvertCurrencyRequest struct {
 	Amount float64 `json:"amount" binding:"required"`
 }
 
+//go:generate mockery --name CurrencyUseCase --output ../mocks/domain --outpkg domain --case=underscore
 type CurrencyUseCase interface {
 	ConvertCurrency(ctx context.Context, from string, to string, amount float64) (float64, error)
 	SaveCurrency(ctx context.Context, from string, to string, rate float64) error
 }
 
+//go:generate mockery --name CurrencyRepository --output ../mocks/domain --outpkg domain --case=underscore
 type CurrencyRepository interface {
 	GetCurrency(ctx context.Context, from string, to string) (float64, error)
 	SaveCurrency(ctx context.Context, from string, to string, rate float64) error
